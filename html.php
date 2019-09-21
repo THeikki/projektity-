7dce485a8726dea6843bda7d045aadc5fedc8764
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-09-21 16:58:20 +0300

    kirjautumissivu tehty

---
+++ html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Kirjautumissivu</title>
+  </head>
+  <body>
+    <label for="k_tunnus">Käyttäjätunnus</label><br>
+    <input type="text" id="k_tunnus" name="k_tunnus" value=""><br>
+    <label for="s_sana">Salasana</label><br>
+    <input type="password" id="s_sana" name="s_sana" value=""><br>
+    <button>Kirjaudu</button>
+  </body>
+</html>
