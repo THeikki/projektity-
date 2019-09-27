22cd9d9e2c8b10c20953840f76aaafc271f7d098
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-09-27 17:42:21 +0300

    css m

---
+++ lainatvälilehti.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Lainat</title>
+  </head>
+  <style>
+  </style>
+  <body>
+    <a class="main_button">VERKKOPANKKI</a>
+    <a class="logout">LOPETA</a><br>
+    <a class="button1">Käyttäjä</a>
+    <a class="button1">Tilit</a>
+    <a class="button1">Sijoitukset</a>
+    <a class="button2">Lainat</a>
+    <hr class="line">
+  </body>
+  <body>
+    <table>
+      <tr>
+        <th>Lainan nimi</th>
+        <th>Lainan ottopäivä</th>
+        <th>Lainan määrä</th>
+        <th>Kuukausierä €</th>
+        <th>Takaisinmaksuaika v</th>
+        <th>Lainan korko %</th>
+      </tr>
+      <tr>
+        <td>Asuntolaina</td>
+        <td>2016-01-01</td>
+        <td>200.00,00€</td>
+        <td>666,66</td>
+        <td>25</td>
+        <td>2,0</td>
+      </tr>
+    </table>
+  <body>
+</html>
