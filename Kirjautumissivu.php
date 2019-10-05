1a78c0f51275e412170dfbc22f330ed2380a30f5
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-05 21:16:51 +0300

    php update

--- Kirjautumissivu.php
+++ Kirjautumissivu.php
@@ -16,9 +16,24 @@
     <div class="box">
       <label>Käyttäjätunnus</label>
       <input type="text" id="input1" name="k_tunnus" value=""><br><br>
+      <form>
       <label>Salasana</label>
-      <input type="password" id="input2" name="s_sana" value=""><br>
+      <input type="password" id="input2">
       <a href="Pääsivu.php"class="login">Kirjaudu</a>
-    </div>
-  </body>
+
+  </form>
+  <!--Function to check password the already set password is admin-->
+  <script type="text/javascript">
+      function checkPswd() {
+          var confirmPassword = "admin";
+          var password = document.getElementById("input2").value;
+          if (password == confirmPassword) {
+               window.location="Pääsivu.php";
+          }
+          else{
+              alert("Väärä salasana");
+          }
+      }
+  </script>
+      </body>
 </html>
