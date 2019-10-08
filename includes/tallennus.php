b91c59d144b8fbe2d583703e37d4f7c98ce5f285
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-08 15:59:57 +0300

    2345

--- includes/tallennus.php
+++ includes/tallennus.php
@@ -1,15 +1,15 @@
 <?php
   include_once 'yhteys.php';
 
-  $idK = mysqli_real_escape_string($conn, $POST['idK']);
-  $etunimi = mysqli_real_escape_string($conn, $POST['etun']);
-  $sukunimi = mysqli_real_escape_string($conn, $POST['sukun']);
-  $osoite = mysqli_real_escape_string($conn, $POST['oso']);
-  $puhelinnumero = mysqli_real_escape_string($conn, $POST['puh']);
+  $idK = mysqli_real_escape_string($conn, $_POST['idK']);
+  $etunimi = mysqli_real_escape_string($conn, $_POST['etun']);
+  $sukunimi = mysqli_real_escape_string($conn, $_POST['sukun']);
+  $osoite = mysqli_real_escape_string($conn, $_POST['oso']);
+  $puhelinnumero = mysqli_real_escape_string($conn, $_POST['puh']);
 
   $sql = "INSERT INTO Kayttaja (idKayttaja, Etunimi, Sukunimi, Osoite, Puhelinnumero)
   VALUES ('$idK', '$etunimi', '$sukunimi', '$osoite','$puhelinnumero')";
   mysqli_query($conn, $sql);
 
-  header("Location: ../lisää_käyttäjä.php?tallennus=success");
+  header("Location: ../Pääsivu.php?tallennus=success");
 ?>
