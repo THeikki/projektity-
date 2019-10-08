57eeb649a0167bfd1af49ed0f1dfc57948fc5e75
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-08 14:58:24 +0300

    jee

--- lisää_sijoitus.php
+++ lisää_sijoitus.php
@@ -12,7 +12,7 @@
     <a href="Pääsivu.php" class="back">Takaisin</a>
     <div class="box" id="box1">
         <h2>Sijoitustiedot</h2>
-        <form class="" action="" methot="post">
+        <form class="" action="" methot="POST">
           <input type="number" name="idS" value="" placeholder="id Sijoitukset"> <br><br>
           <input type="text" name="sijk" value="" placeholder="Sijoituskohde"> <br><br>
           <input type="text" name="sij_m" value="" placeholder="Sijoituksen määrä"> <br><br>
@@ -22,20 +22,17 @@
           <input type="submit" name="" value="Tallenna">
         </form>
     </div>
-  </body>
-  <body>
     <?php
-      $idS = mysqli_real_escape_string($con, $_POST['idS']);
-      $sijoituskohde = mysqli_real_escape_string($con, $_POST['sijk']);
-      $sijoituksen_maara = mysqli_real_escape_string($con, $_POST['sij_m']);
-      $osakkeiden_lukumaara = mysqli_real_escape_string($con, $_POST['osa_l']);
-      $osakkeen_arvo = mysqli_real_escape_string($con, $_POST['osa_a']);
-      $idK = mysqli_real_escape_string($con, $_POST['idK']);
+      $idS = mysqli_real_escape_string($conn, $_POST['idS']);
+      $sijoituskohde = mysqli_real_escape_string($conn, $_POST['sijk']);
+      $sijoituksen_maara = mysqli_real_escape_string($conn, $_POST['sij_m']);
+      $osakkeiden_lukumaara = mysqli_real_escape_string($conn, $_POST['osa_l']);
+      $osakkeen_arvo = mysqli_real_escape_string($conn, $_POST['osa_a']);
+      $idK = mysqli_real_escape_string($conn, $_POST['idK']);
 
       $sql = "INSERT INTO Sijoitukset (idSijoitukset, Sijoituskohde, Sijoituksen maara, Osakkeiden lukumaara, Osakkeen arvo)
       VALUES ('$idL', '$sijoituskohde', '$sijoituksen_maara', '$osakkeiden_lukumaara','$osakkeen_arvo', '$idK')";
-
-      echo $sql;
+      mysqli_query($conn, $sql);
     ?>
   </body>
 </html>
