b94a893ea711db3b327a9e5d18d71f2721afcfdf
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-11 01:18:40 +0300

    986875

--- lisää_laina.php
+++ lisää_laina.php
@@ -3,11 +3,11 @@
 
   if(isset($_POST['insert'])) {
 
-    $idLainat = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idLainat',FILTER_SANITIZE_STRING));
-    $lainannimi = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan nimi',FILTER_SANITIZE_STRING));
-    $lainanmaara = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan maara',FILTER_SANITIZE_STRING));
-    $lainankorko = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan korko',FILTER_SANITIZE_STRING));
-    $idKayttaja = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idKayttaja',FILTER_SANITIZE_STRING));
+    $idLainat = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'',FILTER_SANITIZE_STRING));
+    $lainannimi = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'',FILTER_SANITIZE_STRING));
+    $lainanmaara = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'',FILTER_SANITIZE_STRING));
+    $lainankorko = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'',FILTER_SANITIZE_STRING));
+    $idKayttaja = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'',FILTER_SANITIZE_STRING));
 
     $sqlinsert = "INSERT INTO Lainat (idLainat, Lainan nimi, Lainan maara, Lainan korko, idKayttaja)
     VALUES ('$idlainat', '$lainannimi', '$lainanmaara', '$lainankorko', '$idKayttaja')";
@@ -34,11 +34,11 @@
     <div id="box1">
         <h2>Lainatiedot</h2>
         <form action="lisää_laina.php" method="post">
-          <input type="number" name="idLainat" value="" placeholder="id Laina"> <br><br>
-          <input type="text" name="Lainan nimi" value="" placeholder="Lainan nimi"> <br><br>
-          <input type="number" name="Lainan maara" value="" placeholder="Lainan määrä"> <br><br>
-          <input type="number" name="Lainan korko" value="" placeholder="Lainan korko"> <br><br>
-          <input type="number" name="idKayttaja" value="" placeholder="id Käyttäjä"> <br><br>
+          <input type="number" name="" value="" placeholder="id Laina"> <br><br>
+          <input type="text" name="" value="" placeholder="Lainan nimi"> <br><br>
+          <input type="number" name="" value="" placeholder="Lainan määrä"> <br><br>
+          <input type="number" name="" value="" placeholder="Lainan korko"> <br><br>
+          <input type="number" name="" value="" placeholder="id Käyttäjä"> <br><br>
           <input type="submit" name="insert" value="Tallenna"> <br>
         </form>
     </div>
