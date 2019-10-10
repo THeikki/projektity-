6e37c748880a48d4bcc3a5fde18e8a9075fd7003
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-10 16:45:02 +0300

    fft

--- lisää_käyttäjä.php
+++ lisää_käyttäjä.php
@@ -1,11 +1,11 @@
 <?php
   include "includes/yhteys.php";
 
-  $idK = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'idKayttaja',FILTER_SANITIZE_STRING));
-  $etunimi = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'Etunimi',FILTER_SANITIZE_STRING));
-  $sukunimi = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'Sukunimi',FILTER_SANITIZE_STRING));
-  $osoite = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'Osoite',FILTER_SANITIZE_STRING));
-  $puhelinnumero = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'Puhelinnumero',FILTER_SANITIZE_STRING));
+  $idK = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idKayttaja',FILTER_SANITIZE_STRING));
+  $etunimi = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Etunimi',FILTER_SANITIZE_STRING));
+  $sukunimi = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Sukunimi',FILTER_SANITIZE_STRING));
+  $osoite = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Osoite',FILTER_SANITIZE_STRING));
+  $puhelinnumero = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Puhelinnumero',FILTER_SANITIZE_STRING));
 
   $sqlinsert = "INSERT INTO Kayttaja (idKayttaja, Etunimi, Sukunimi, Osoite, Puhelinnumero)
   VALUES ('$idK', '$etunimi', '$sukunimi', '$osoite', '$puhelinnumero')";
--- lisää_sijoitus.php
+++ lisää_sijoitus.php
@@ -1,12 +1,12 @@
 <?php
   include "includes/yhteys.php";
 
-  $idS = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'idSijoitukset',FILTER_SANITIZE_STRING));
-  $sijoituskohde = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'Sijoituksen kohde',FILTER_SANITIZE_STRING));
-  $sijoituksenmaara = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'Sijoituksen maara',FILTER_SANITIZE_STRING));
-  $osakkeidenlukumaara = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'Osakkeiden lukumaara',FILTER_SANITIZE_STRING));
-  $osakkeenarvo = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'Osakkeen arvo',FILTER_SANITIZE_STRING));
-  $idK = mysqli_real_escape_stringg($conn, filter_input(INPUT_POST,'idKayttaja',FILTER_SANITIZE_STRING));
+  $idS = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idSijoitukset',FILTER_SANITIZE_STRING));
+  $sijoituskohde = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Sijoituksen kohde',FILTER_SANITIZE_STRING));
+  $sijoituksenmaara = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Sijoituksen maara',FILTER_SANITIZE_STRING));
+  $osakkeidenlukumaara = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Osakkeiden lukumaara',FILTER_SANITIZE_STRING));
+  $osakkeenarvo = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Osakkeen arvo',FILTER_SANITIZE_STRING));
+  $idK = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idKayttaja',FILTER_SANITIZE_STRING));
 
   $sqlinsert = "INSERT INTO Sijoitukset (idSijoitukset, Sijoituskohde, Sijoituksen maara, Osakkeiden lukumaara, Osakkeen arvo, idKayttaja)
   VALUES ('$idS', '$sijoituskohde', '$sijoituksenmaara', '$osakkeidenlukumaara','$osakkeenarvo', '$idK')";
