05dff856cc557d4b6e73fa0ee322be157e35aa1b
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-10 18:41:32 +0300

    uji

--- lisää_laina.php
+++ lisää_laina.php
@@ -2,9 +2,9 @@
 <?php
 
   $idLainat = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idLainat',FILTER_SANITIZE_STRING));
-  $lainannimi = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan nimi',FILTER_SANITIZE_STRING));
-  $lainanmaara = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan maara',FILTER_SANITIZE_STRING));
-  $lainankorko = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan korko',FILTER_SANITIZE_STRING));
+  $Lainannimi = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainannimi',FILTER_SANITIZE_STRING));
+  $Lainanmaara = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainanmaara',FILTER_SANITIZE_STRING));
+  $Lainankorko = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainankorko',FILTER_SANITIZE_STRING));
   $idKayttaja = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idKayttaja',FILTER_SANITIZE_STRING));
 
   $sqlinsert = "INSERT INTO Lainat (idLainat, Lainan nimi, Lainan maara, Lainan korko, idKayttaja)
@@ -25,9 +25,9 @@
         <h2>Lainatiedot</h2>
         <form action"lisää_laina.php" method="post">
           <input type="number" name="idLainat" value="" placeholder="id Laina"> <br><br>
-          <input type="text" name="Lainan nimi" value="" placeholder="Lainan nimi"> <br><br>
-          <input type="text" name="Lainan maara" value="" placeholder="Lainan määrä"> <br><br>
-          <input type="text" name="Lainan korko" value="" placeholder="Lainan korko"> <br><br>
+          <input type="text" name="Lainannimi" value="" placeholder="Lainan nimi"> <br><br>
+          <input type="text" name="Lainanmaara" value="" placeholder="Lainan määrä"> <br><br>
+          <input type="text" name="Lainankorko" value="" placeholder="Lainan korko"> <br><br>
           <input type="number" name="idKayttaja" value="" placeholder="id Käyttäjä"> <br><br>
           <input type="submit" name="" value="Tallenna">
         </form>
