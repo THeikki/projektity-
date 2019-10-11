f8f6c9b57ce9499b6288da70b09716d09fb5033c
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-11 10:05:28 +0300

    ggd

--- lisää_laina.php
+++ lisää_laina.php
@@ -4,37 +4,41 @@
 
   if(isset($_POST['insert'])) {
 
-    $idLainat = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idLainat',FILTER_SANITIZE_STRING));
-    $lainannimi = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan_nimi',FILTER_SANITIZE_STRING));
-    $lainanmaara = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan_maara',FILTER_SANITIZE_STRING));
-    $lainankorko = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan_korko',FILTER_SANITIZE_STRING));
+    $idLainat = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idLainatt',FILTER_SANITIZE_STRING));
+    $lainan_nimi = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan_nimi',FILTER_SANITIZE_STRING));
+    $lainan_maara = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan_maara',FILTER_SANITIZE_STRING));
+    $lainan_korko = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Lainan_korko',FILTER_SANITIZE_STRING));
     $idKayttaja = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idKayttaja',FILTER_SANITIZE_STRING));
 
     $sqlinsert = "INSERT INTO Lainat (idLainat, Lainan_nimi, Lainan_maara, Lainan_korko, idKayttaja)
-    VALUES ('$idlainat', '$lainannimi', '$lainanmaara', '$lainankorko', '$idKayttaja')";
+    VALUES ('$idLainat', '$lainan_nimi', '$lainan_maara', '$lainan_korko', '$idKayttaja')";
 
-    mysqli_query($conn, $sqlinsert);
+    $query_run = mysqli_query($conn, $sqlinsert);
 
-    if (!mysqli_query($conn, $sqlinsert)) {
-      die('Error: ' . mysqli_error($conn));
+    if($query_run) {
+      echo '<script> alert("Uusi laina tallennettiin")</script>';
     }
+  else {
+    echo '<script> alert("Lainaa ei tallennettu")</script>';
+  }
+  }
 ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>lisää_laina</title>
+    <title>lisää_sijoitus</title>
     <link rel="stylesheet" href="includes/mystyle.css">
   </head>
   <body>
     <hr class="line">
     <a href="Pääsivu.php" class="back">Takaisin</a>
-    <div id="box1">
-        <h2>Lainatiedot</h2>
-        <form action="lisää_laina.php" method="post">
-          <input type="number" name="idLainat" value="" placeholder="id Laina"> <br><br>
-          <input type="text" name="Lainan_nimi" value="" placeholder="Lainan nimi"> <br><br>
-          <input type="text" name="Lainan_maara" value="" placeholder="Lainan määrä"> <br><br>
-          <input type="text" name="Lainan_korko" value="" placeholder="Lainan korko"> <br><br>
+    <div class="box" id="box1">
+        <h2>Sijoitustiedot</h2>
+        <form action="lisää_sijoitus.php" method="post">
+          <input type="number" name="idLainat" value="" placeholder="id Sijoitus"> <br><br>
+          <input type="text" name="Lainan_nimi" value="" placeholder="Sijoituskohde"> <br><br>
+          <input type="text" name="Lainan_maara" value="" placeholder="Sijoituksen määrä"> <br><br>
+          <input type="text" name="Lainan_korko" value="" placeholder="Osakkeiden lukumäärä"> <br><br>
           <input type="number" name="idKayttaja" value="" placeholder="id Käyttäjä"> <br><br>
           <input type="submit" name="insert" value="Tallenna"> <br>
         </form>
