4480b62246ddb3e7c85c7489837dd07006d4cdbc
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-12 02:20:25 +0300

    meeehuuyy

--- muokkaa_käyttäjä.php
+++ muokkaa_käyttäjä.php
@@ -9,31 +9,29 @@ if(isset($_POST['update'])) {
 }
 
 
-$sql = "SELECT * FROM Kayttaja";
-$mydata = mysqli_query($conn, $sql);
-echo "<table border=2>
-<tr>
-<th>idKayttaja</th>
-<th>Etunimi</th>
-<th>Sukunimi</th>
-<th>Osoite</th>
-<th>Puhelinnumero</th>
-</tr>";
+$sql = "SELECT * FROM Kayttaja;";
+$result = mysqli_query($conn, $sql);
 
+echo "<table border='1'>
+  <tr>
+  <th>idKayttaja</th>
+  <th>Etunimi</th>
+  <th>Sukunimi</th>
+  <th>Osoite</th>
+  <th>Puhelinnumero</th>
+  </tr>";
 
-while($record = mysqli_fetch_array($mydata)) {
-echo "<form action=muokkaa_käyttäjä.php method=post>";
-echo "<tr>";
-echo "<td>" . "<input type=number name=idKayttaja value=" . $record['idKayttaja'] . " </td>";
-echo "<td>" . "<input type=text name=etunimi value=" . $record['Etunimi'] . " </td>";
-echo "<td>" . "<input type=text name=sukunimi value=" . $record['Sukunimi'] . " </td>";
-echo "<td>" . "<input type=text name=osoite value=" . $record['Osoite'] . " </td>";
-echo "<td>" . "<input type=text name=puhelinnumero value=" . $record['Puhelinnumero'] . " </td>";
-echo "<td>" . "<input type=hidden name=idKayttaja value=" . $record['idKayttaja'] . " </td>";
-echo "<td>" . "<input type=submit name=update value=Muokkaa" . " </td>";
-echo "</tr>";
-echo "</form>";
-
-echo "</table>";
-
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['idKayttaja'] . "</td>";
+  echo "<td>" . $row['Etunimi'] . "</td>";
+  echo "<td>" . $row['Sukunimi'] . "</td>";
+  echo "<td>" . $row['Osoite'] . "</td>";
+  echo "<td>" . $row['Puhelinnumero'] . "</td>";
+  echo "<td><a href=muokkaa_käyttäjä.php?update=" . $row['idKayttaja'] . ">Muokkaa</a></td>";
+  echo "<td><a href=poista_käyttäjä.php?delete=" . $row['idKayttaja'] . ">Poista</a></td>";
+  echo "</tr>";
+  echo "</form>";
+  }
+  echo "</table>";
 ?>
