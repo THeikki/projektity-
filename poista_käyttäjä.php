70bc1af4c679a514e5e8b29b6646534e3205aa1c
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-11 20:15:02 +0300

    wwsdfr32

--- poista_käyttäjä.php
+++ poista_käyttäjä.php
@@ -2,11 +2,9 @@
 ?>
 <?php
 
-
-
   if(isset($_GET['del'])) {
     $idKayttaja = $_GET['del'];
     $Deletequery = "DELETE FROM Kayttaja WHERE idKayttaja='$idKayttaja'";
-    @result = mysqli_query($conn, $Deletequery) or die("Virhe". mysqli_error());
+    $result = mysqli_query($conn, $Deletequery) or die("Virhe". mysqli_error());
   }
 ?>
