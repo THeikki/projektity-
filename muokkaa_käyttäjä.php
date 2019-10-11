d8cd3954c8b254adea20274d0054670a185b6e8b
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-11 23:28:48 +0300

    lllllll

--- muokkaa_käyttäjä.php
+++ muokkaa_käyttäjä.php
@@ -9,14 +9,14 @@
 		$row= mysqli_fetch_array($res);
 	}
 
-	if( isset($_POST['newName']))
+	if( isset($_POST['update']))
 	{
-    $idKayttaja = $_POST['idKayttaja'];
-    $uusi_etun = $_POST['uusi_etun'];
-    $uusi_sukun = $_POST['uusi_sukun'];
-    $uusi_os = $_POST['uusi_os'];
-    $uusi_puh = $_POST['uusi_puh'];
-		$Updatequery = "UPDATE Kayttaja SET Etunimi='$uusi_etun', Sukunimi='$uusi_sukun', Osoite='$uusi_os', Puhelinnumero='$uusi_puh' WHERE id='$idKayttaja'";
+    $idKayttaja = $_GET['idKayttaja'];
+    $etunimi = $_POST['Etunimi'];
+    $sukunimi = $_POST['Sukunimi'];
+    $osoite = $_POST['Osoite'];
+    $ouhelinnumero = $_POST['Puhelinnumero'];
+		$Updatequery = "UPDATE Kayttaja SET Etunimi='$etunimi', Sukunimi='$sukunimi', Osoite='$osoite', Puhelinnumero='$puhelinnumero' WHERE id='$idKayttaja'";
 		mysqli_query($conn, $Updatequery) or die mysqli_error($conn));
 		  echo "Käyttäjää muokattiin onnistuneesti!";
 	}
@@ -34,12 +34,12 @@
     <div id="box1">
         <h2>Muokkaa käyttäjätietoja</h2>
         <form action="muokkaa_käyttäjä.php" method="post">
-          <input type="hidden" name="idKayttaja" value="" placeholder="id Käyttäjä"> <br><br>
-          <input type="text" name="Etunimi" value="" placeholder="Etunimi"> <br><br>
-          <input type="text" name="Sukunimi" value="" placeholder="Sukunimi"> <br><br>
-          <input type="text" name="Osoite" value="" placeholder="Osoite"> <br><br>
-          <input type="number" name="Puhelinnumero" value="" placeholder="Puhelinnumero"> <br><br>
-          <input type="submit" name="insert" value="Muokkaa"> <br>
+          <input type="hidden" name="idKayttaja" value="<?php echo $idKayttaja ?>" placeholder="id Käyttäjä"> <br><br>
+          <input type="text" name="Etunimi" value="<?php echo $etunimi ?>" placeholder="Etunimi"> <br><br>
+          <input type="text" name="Sukunimi" value="<?php echo $sukunimi ?>" placeholder="Sukunimi"> <br><br>
+          <input type="text" name="Osoite" value="<?php echo $osoite ?>" placeholder="Osoite"> <br><br>
+          <input type="number" name="Puhelinnumero" value="<?php echo $puhelinnumero ?>" placeholder="Puhelinnumero"> <br><br>
+          <input type="submit" name="update" value="Muokkaa"> <br>
         </form>
     </div>
   </body>
