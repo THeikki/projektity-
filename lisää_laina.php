3fbdb6265a0fe55bca45bd59ded21e57d34c7161
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-11 11:02:21 +0300

    jee se toimii

--- lisää_laina.php
+++ lisää_laina.php
@@ -13,12 +13,15 @@
     $sqlinsert = "INSERT INTO Lainat (idLainat, Lainan_nimi, Lainan_maara, Lainan_korko, idKayttaja)
     VALUES ('$idLainat', '$lainan_nimi', '$lainan_maara', '$lainan_korko', '$idKayttaja')";
 
-    mysqli_query($conn, $sqlinsert);
+    $query_run = mysqli_query($conn, $sqlinsert);
 
-    if (!mysqli_query($conn, $sqlinsert)) {
-     die('Error: ' . mysqli_error($conn));
+    if($query_run) {
+      echo '<script> alert("Uusi käyttäjä tallennettiin")</script>';
     }
- }
+  else {
+    echo '<script> alert("Käyttäjää ei tallennettu")</script>';
+  }
+  }
 ?>
 <html lang="en" dir="ltr">
   <head>
