e590e6da3c863e9988e3ca5924d21796531b10dc
Heikki Törmänen <t9tohe00@students.oamk.fi>
2019-10-12 11:58:17 +0300

    bbbbbb

--- muokkaa_käyttäjä.php
+++ muokkaa_käyttäjä.php
@@ -1,11 +1,23 @@
 <?php include "includes/yhteys.php"
 ?>
 <?php
+if(isset($_GET['edit'])){
+  $sql = "SELECT * FROM Kayttaja WHERE idKayttaja =" .$_GET['update'];
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($result);
+  }
 
 if(isset($_POST['update'])) {
-        $Updatequery = "UPDATE Kayttaja SET idKayttaja='$_POST[idKayttaja]', Etunimi='$_POST[Etunimi]', Sukunimi='$_POST[Sukunimi]', Osoite='$_POST[Osoite]', Puhelinnumero='$_POST[Puhelinnumero]' WHERE idKayttaja='$_POST[hidden]'";
-        mysqli_query($conn, $Updatequery) or die(mysqli_error($conn));
-          echo "Käyttäjä muokattiin onnistuneesti!";
+
+  $idkayttaja = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'idKayttaja',FILTER_SANITIZE_STRING));
+  $etunimi = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Etunimi',FILTER_SANITIZE_STRING));
+  $sukunimi = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Sukunimi',FILTER_SANITIZE_STRING));
+  $osoite = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Osoite',FILTER_SANITIZE_STRING));
+  $puhelinnumero = mysqli_real_escape_string($conn, filter_input(INPUT_POST,'Puhelinnumero',FILTER_SANITIZE_STRING));
+
+  $Updatequery = "UPDATE Kayttaja SET idKayttaja='$idkayttaja', Etunimi='$etunimi', Sukunimi='$sukunimi', Osoite='$osoite', Puhelinnumero='$puhelinnumero' WHERE idKayttaja='$_POST[hidden]'";
+  mysqli_query($conn, $Updatequery) or die(mysqli_error($conn));
+    echo "Käyttäjä muokattiin onnistuneesti!";
 }
 ?>
 <html lang="en" dir="ltr">
@@ -20,11 +32,11 @@ if(isset($_POST['update'])) {
     <div id="box1">
         <h2>Muokkaa käyttäjätietoja</h2>
         <form action="muokkaa_käyttäjä.php" method="post">
-          <input type="hidden" name="idK" value="" placeholder="id Käyttäjä"> <br><br>
-          <input type="text" name="etunimi" value="" placeholder="Etunimi"> <br><br>
-          <input type="text" name="sukunimi" value="" placeholder="Sukunimi"> <br><br>
-          <input type="text" name="osoite" value="" placeholder="Osoite"> <br><br>
-          <input type="number" name="puhelinnumero" value="" placeholder="Puhelinnumero"> <br><br>
+          <input type="hidden" name="idKayttaja" placeholder="id Käyttäjä" value="<?php echo $row['idKayttaja']; ?>"><br/><br/>
+          <input type="text" name="Etunimi" placeholder="Etunimi" value="<?php echo $row['Etunimi']; ?>"><br/><br/>
+          <input type="text" name="Sukunimi" placeholder="Sukunimi" value="<?php echo $row['Sukunimi']; ?>"><br/><br/>
+          <input type="text" name="Osoite" placeholder="Osoite" value="<?php echo $row['Osoite']; ?>"><br/><br/>
+          <input type="number" name="Puhelinnumero" placeholder="Puhelinnumero" value="<?php echo $row['Puhelinnumero']; ?>"><br/><br/>
           <input type="submit" name="update" value="Muokkaa"> <br>
         </form>
     </div>
